ec7aa66a6c76064cbba5172cddbaae16ad38730c
wgzesg <wgzesg@gmail.com>
2020-10-04 17:56:08 +0800

    statge parser

--- src/main/java/workout/workoutsession/WorkoutSessionParser.java
+++ src/main/java/workout/workoutsession/WorkoutSessionParser.java
@@ -1,32 +1,21 @@
-package workout.workoutsession;
+package workout.workoutSession;
 
-import workout.workoutsession.exercise.Exercise;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class WorkoutSessionParser {
+    public static void workoutSessionProcessCommand(){
+        Scanner in = new Scanner(System.in);
+        ArrayList<String> input = new ArrayList<String>;
+        workoutSessionParser(in.nextLine().trim(), input);
 
-    public static String[] workoutSessionParser(String input) {
+        switch (input.toLowerCase(){
+        case "add":
 
-        return input.split(" ");
-    }
-
-    public static Exercise addParser(String[] input) {
-
-        String[] returnString = new String[3];
-        int tracker = 0;
-        for (String s : input) {
-            if (s.compareTo("/n") == 0) {
-                tracker = 1;
-            } else if (s.compareTo("/w") == 0) {
-                tracker = 2;
-            } else {
-                returnString[tracker] += s;
-            }
         }
-        return new Exercise(returnString[0],Integer.parseInt(returnString[1]),Integer.parseInt(returnString[2]));
     }
 
-    public static int deleteParser(String[] input) {
-
-        return Integer.parseInt(input[1]);
+    private static void workoutSessionParser(String input, ArrayList<String> output){
+        output
     }
 }
