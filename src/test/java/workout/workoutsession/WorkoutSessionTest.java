d74dbb9c2eded445fa70786ffecb279fce21792d
yujinyang1998 <56167146+yujinyang1998@users.noreply.github.com>
2020-10-15 18:34:33 +0800

    Revert "fix gradle"

--- src/test/java/workout/workoutsession/WorkoutSessionTest.java
+++ src/test/java/workout/workoutsession/WorkoutSessionTest.java
@@ -2,10 +2,13 @@ package workout.workoutsession;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class WorkoutSessionTest {
 
     @Test
     void workoutSessionStart() {
-
+            WorkoutSession ws = new WorkoutSession();
+            ws.workoutSessionStart();
     }
 }
